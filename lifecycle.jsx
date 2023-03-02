4df43f18d5ec40b8faa57d5dc0589b58e17b12e8
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-03-02 22:14:15 +0800

    Update lifecycle.jsx

--- lifecycle.jsx
+++ lifecycle.jsx
@@ -8,32 +8,35 @@ const Square = ({ takeTurn, id }) => {
     const [tik, setTik] = React.useState(2);
   
     // Step 1: Move the mounted state & toggle from the board component to here
+    const [mounted, setMounted] = React.useState(true);
+    const toggle = () => setMounted(!mounted);
   
     // Step 2: Check to see if the mounted state is false. If it is we want to return null instead of returning the button
+    if (!mounted) {
+        return null;
+    }
   
     return (
-      <button
-        // DO NOT DELETE THIS id
-        id={`square-button-${id}`}
-        onClick={() => {
-          setTik(takeTurn(id));
-          setFilled(true);
-          // Step 3: Trigger toggle() when button is clicked
-        }}
-      >
+        <button
+            // DO NOT DELETE THIS id
+            id={`square-button-${id}`}
+            onClick={() => {
+                setTik(takeTurn(id));
+                setFilled(true);
+                // Step 3: Trigger toggle() when button is clicked
+                toggle();
+            }}
+        >
         <h1>{mark[tik]}</h1>
-      </button>
+        </button>
     );
-  };
-  
-  const Board = () => {
+};
+
+const Board = () => {
     // 1st player is X ie 1
     // State keeps track of next player and gameState
     const [player, setPlayer] = React.useState(1);
-    const [mounted, setMounted] = React.useState(true);
     const [gameState, setGameState] = React.useState([]);
-  
-    const toggle = () => setMounted(!mounted);
   
     const takeTurn = (id) => {
       setGameState([...gameState, { id: id, player: player }]);
@@ -44,32 +47,31 @@ const Square = ({ takeTurn, id }) => {
       // use properties to pass callback function takeTurn to Child
       return <Square takeTurn={takeTurn} id={i}></Square>;
     }
-  
+
     return (
-      <div className="game-board">
-        <div className="grid-row">
-          {mounted && renderSquare(0)}
-          {mounted && renderSquare(1)}
-          {mounted && renderSquare(2)}
+        <div className="game-board">
+            <div className="grid-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div id="info">
+                <h1>{status}</h1>
+            </div>
         </div>
-        <div id="info">
-          <button onClick={toggle}>Show/Hide Row</button>
-          <h1>{status}</h1>
-        </div>
-      </div>
     );
-  };
-  
-  const Game = () => {
+};
+
+const Game = () => {
     return (
-      <div className="game">
-        <Board></Board>
-      </div>
+        <div className="game">
+            <Board></Board>
+        </div>
     );
-  };
-  
-  // ========================================
-  
+};
+
+// ========================================
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const element = <Game/>
+const element = <App/>
 root.render(element);
